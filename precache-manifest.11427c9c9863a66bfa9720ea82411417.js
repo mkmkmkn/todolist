e5c17033318a0623e23b8012c3125961724c849e
unknown <mkgoma3@gmail.com>
2020-08-04 16:57:41 +0900

    Updates

---
+++ precache-manifest.11427c9c9863a66bfa9720ea82411417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b92d732bb76edc9b8f77b35935a9d1e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "103f8be6056fbfb60d92",
+    "url": "/todolist/static/css/main.bce0d1fe.chunk.css"
+  },
+  {
+    "revision": "a4190937d921065ec4a8",
+    "url": "/todolist/static/js/2.887a3615.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.887a3615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103f8be6056fbfb60d92",
+    "url": "/todolist/static/js/main.afec880b.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
